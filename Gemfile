0c26b9b0560189cf02835230ffb3914a112891e6
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-06-21 20:24:50 +0200

    Switched to Rail for front-end development

--- Gemfile
+++ Gemfile
@@ -1,13 +1,5 @@
 source 'https://rubygems.org'
 
-group :development do
-  gem 'rake', '~> 10.3.2'
-
-  gem 'sprockets', '~> 2.12.1'
-
-  gem 'haml', '~> 4.0.5'
-  gem 'redcarpet', '~> 3.1.2'
-  gem 'sass', '~> 3.3.8'
-
-  gem 'thin', '~> 1.6.2'
-end
+gem 'rail', '~> 0.0.2'
+gem 'redcarpet', '~> 3.1.2'
+gem 'thin', '~> 1.6.2'
--- config/application.rb
+++ config/application.rb
@@ -1,37 +1,9 @@
-require 'haml'
-require 'sprockets'
+require 'bundler'
+Bundler.require(:default)
 
-Haml::Options.defaults[:ugly] = true if ENV['production']
-Sprockets.register_engine('.haml', Tilt::HamlTemplate)
-
-class Application
-  def call(env)
-    case env['PATH_INFO']
-    when '/'
-      pipeline = build_pipeline(env)
-      [ 200, {}, pipeline['application.html'] ]
-    when /^.+(css)$/
-      pipeline = build_pipeline(env)
-      pipeline.call(env)
-    else
-      browser.call(env)
-    end
-  end
-
-  private
-
-  def browser
-    @browser ||= Rack::Directory.new('public')
-  end
-
-  def build_pipeline(env)
-    pipeline = Sprockets::Environment.new
-
-    pipeline.append_path('app/assets/stylesheets')
-    pipeline.append_path('app/views/layouts')
-
-    pipeline.css_compressor = :scss if ENV['production']
-
-    pipeline
+module Profile
+  class Application < Rail::Application
+    config.precompile << 'application.css'
+    config.precompile << 'index.html'
   end
 end
